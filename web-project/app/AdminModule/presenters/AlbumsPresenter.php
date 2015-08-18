4a9ed977d52504ba6e463cd58d6c12135f2fde5e
Michal Mlejnek <chaemil72@gmail.com>
2015-08-18 22:34:57 +0200

    added photos manager, albums presenter

---
+++ web-project/app/AdminModule/presenters/AlbumsPresenter.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\AdminModule;
+
+use Nette,
+ Model\PhotosManager;
+
+/**
+ * Description of AlbumsPresenter
+ *
+ * @author chaemil
+ */
+class AlbumsPresenter {
+    
+    public $database;
+    private $photosManager;
+
+    function __construct(Nette\Database\Context $database, PhotosManager $photosManager) {
+        $this->database = $database;
+        $this->photosManager = $photosManager;
+    }
+    
+    public function renderList() {
+        $paginator = new Nette\Utils\Paginator;
+        $paginator->setItemCount($this->photosManager->countVideos());
+        $paginator->setItemsPerPage(30);
+        $paginator->setPage(1);
+        
+        $this->getTemplateVariables($this->getUser()->getId());
+        
+        $this->template->albums = $this->photosManager
+                ->getAlbumsFromDB($paginator->getLength(), $paginator->getOffset(), "id");
+    }
+    
+    public function renderAlbumDetail($id) {
+        $this->getTemplateVariables($this->getUser()->getId());
+        $album = $this->photosManager->getAlbumFromDB($id);
+        
+        $this->template->album = $album;
+    }
+    
+    public function createComponentVideoBasicInfoForm() {        
+        $form = new Nette\Application\UI\Form;
+        
+        $form->addHidden('id')
+                ->setRequired();
+        
+        $published = array(
+            '0' => 'Ne',
+            '1' => 'Ano',
+        );
+        
+        $form->addSelect("published", "zveřejneno")
+                ->setItems($published)
+                ->setAttribute("class", "form-control");
+        
+        $form->addText('name_cs', 'název česky')
+                ->setRequired()
+                ->setAttribute("class", "form-control");
+
+        $form->addText('name_en', 'název anglicky')
+                ->setRequired()
+                ->setAttribute("class", "form-control");
+        
+        $form->addText('date', 'datum')
+                ->setRequired()
+                ->setHtmlId("datepicker")
+                ->setAttribute("class", "form-control");
+        
+        $form->addText('days', 'dny')
+                ->setAttribute("type", "number")
+                ->setAttribute("step", "1")
+                ->setAttribute("class", "form-control");
+        
+        $form->addSubmit("submit", "uložit")
+                ->setAttribute("class", "btn-lg btn-success btn-block");
+        
+
+        // call method signInFormSucceeded() on success
+        $form->onSuccess[] = $this->albumInfoSucceeded;
+
+        // setup Bootstrap form rendering
+        $this->bootstrapFormRendering($form);
+
+        return $form;
+    }
+    
+    public function albumInfoSucceeded($form) {
+        $vals = $form->getValues();
+        
+        $status = $this->photosManager->saveAlbumToDB($vals);
+        
+        if ($status) {
+            $this->flashMessage("Změny úspěšně uloženy", "success");
+        } else {
+            $this->flashMessage("Nic nebylo změněno", "info");
+        }
+    }
+}
--- web-project/app/model/PhotosManager.php
+++ web-project/app/model/PhotosManager.php
@@ -6,11 +6,79 @@
  * and open the template in the editor.
  */
 
+namespace Model;
+
+use Nette,
+ App\FileUtils,
+ App\StringUtils;
+
 /**
  * Description of PhotosManager
  *
  * @author chaemil
  */
 class PhotosManager {
-    //put your code here
+    
+    const
+            TABLE_NAME_PHOTOS = 'db_photo_files',
+            COLUMN_ID = 'id',
+            COLUMN_ALBUM_DI = 'album_id',
+            COLUMN_FILE = 'file',
+            COLUMN_DESCRIPTION_CS = 'description_cs',
+            COLUMN_DESCRIPTION_EN = 'description_en',
+            TABLE_NAME_ALBUMS = 'db_albums',
+            COLUMN_HASH = 'hash',
+            COLUMN_PUBLISHED = 'published',
+            COLUMN_NAME_EN = 'name_en',
+            COLUMN_NAME_CS = 'name_cs',
+            COLUMN_DATE = 'date',
+            COLUMN_DAYS = 'days';
+            
+    
+    /** @var Nette\Database\Context */
+    public static $database;
+
+    public function __construct(Nette\Database\Context $database) {
+        $this::$database = $database;
+    }
+    
+    public function saveAlbumToDB($values) {
+        if(isset($values['id'])) {
+            $albumId = \Nette\Utils\Strings::webalize($values['id']);
+        } else {
+            $albumId = 0;
+        }
+        
+        if ($albumId) {
+            $album = $this::$database->table(self::TABLE_NAME_ALBUMS)->get($albumId);
+            $sql = $album->update($values);
+            return $albumId;
+        } else {
+            $values['hash'] = StringUtils::rand(8);
+            $sql = $this::$database->table(self::TABLE_NAME_ALBUMS)->insert($values);
+            $newAlbumDir = ALBUMS_FOLDER.$sql->id."/";
+            $newAlbumThumbsDir = $newAlbumDir."thumbs/";
+            mkdir($newAlbumDir);
+            mkdir($newAlbumThumbsDir);
+            chmod($newAlbumDir, 0777);
+            chmod($newAlbumThumbsDir, 0777);
+        }
+        
+        return $sql->id;
+    }
+    
+    public function getAlbumFromDB($id) {
+        return $this::$database->table(self::TABLE_NAME_ALBUMS)
+                ->select("*")->where(self::COLUMN_ID, $id)->fetch();
+    }
+    
+    public function countAlbums() {
+        return $this::$database->table(self::TABLE_NAME_ALBUMS)->count("*");
+    }
+    
+    public function getAlbumsFromDB($from, $count, $order) {
+        return $this::$database->table(self::TABLE_NAME_ALBUMS)
+                ->limit($from, $count)
+                ->order($order);
+    }
 }
