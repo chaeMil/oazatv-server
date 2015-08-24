ed1870e178b3dbf886f71973789ebfebb9c1fcfe
Michal Mlejnek <chaemil72@gmail.com>
2015-08-24 14:02:58 +0200

    fixed missing argument
    Conflicts:
    	web-project/.gitignore

--- web-project/app/AdminModule/presenters/AccountPresenter.php
+++ web-project/app/AdminModule/presenters/AccountPresenter.php
@@ -105,7 +105,7 @@ class AccountPresenter extends BaseSecuredPresenter {
         $filename = $user_id.".jpg";
         $vals->avatar->move($this->userAvatarsDirectory.$filename);
 
-        ImageUtils::resizeImage($this->userAvatarsDirectory, $filename, 300, "");
+        ImageUtils::resizeImage($this->userAvatarsDirectory, $filename, 300, "", $this->userAvatarsDirectory);
         
         $this->flashMessage("Avatar úspěšně změněn", "info");
         $this->redirect("Account:default");
