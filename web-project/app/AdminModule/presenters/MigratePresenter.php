ef5bad15e09ba3de6b3899b8e2579ba28d9dab2e
Michal Mlejnek <chaemil72@gmail.com>
2015-10-06 22:40:17 +0200

    updated migration script

--- web-project/app/AdminModule/presenters/MigratePresenter.php
+++ web-project/app/AdminModule/presenters/MigratePresenter.php
@@ -13,6 +13,7 @@ use Nette,
 class MigratePresenter extends BasePresenter {
     public $database;
     public $videoManager;
+    public $videoFolderPrefix = 'db/videos/';
     
     function __construct(Nette\Database\Context $database,
             VideoManager $videoManager) {
@@ -23,7 +24,17 @@ class MigratePresenter extends BasePresenter {
     function actionMigrate($id) {
         $video = $this->videoManager->getVideoFromDB($id, 2);
         $this->template->video = $video;
+        $maxVideoId = $this->videoManager->countVideos();
+        $this->template->maxVideoId = $maxVideoId;
+        $this->template->previousVideoId = $video['id'] - 1;
+        $nextVideoId = $video['id'] + 1;
         
+        if ($this->videoManager->getVideoFromDB($nextVideoId)) {
+            $this->template->nextVideoId = $nextVideoId;
+        } else {
+            $this->template->nextVideoId = $nextVideoId + 1;
+        }
+
         $videoNew['id'] = $video['id'];
         $videoNew['published'] = $video['published'];
         $videoNew['original_file'] = '';
@@ -40,7 +51,50 @@ class MigratePresenter extends BasePresenter {
         } else {
             $videoNew['mp4_file'] = '';
         }
-       
+        
+        $mp4File = substr($video['original_file'], 0, -4).'.mp4';
+        
+        if (file_exists($this->videoFolderPrefix.$mp4File)) {
+            $videoNew['mp4_file'] = $mp4File;
+        } else {
+            $videoNew['mp4_file'] = '';
+        }
+        
+        $webmFile = substr($video['original_file'], 0, -4).'.webm';
+        
+        if (file_exists($this->videoFolderPrefix.$webmFile)) {
+            $videoNew['webm_file'] = $webmFile;
+        } else {
+            $videoNew['webm_file'] = '';
+        }
+        
+        $mp3File = substr($video['original_file'], 0, -4).'.mp3';
+        
+        if (file_exists($this->videoFolderPrefix.$mp3File)) {
+            $videoNew['mp3_file'] = $mp3File;
+        } else {
+            $videoNew['mp3_file'] = '';
+        }
+        
+        $thumbFile = substr($video['original_file'], 0, -4).'.jpg';
+        
+        if (file_exists($this->videoFolderPrefix.$thumbFile)) {
+            $videoNew['thumb_file'] = $thumbFile;
+        } else {
+            $videoNew['thumb_file'] = '';
+        }
+        
+        $videoNew['date'] = $video['date'];
+        $videoNew['name_cs'] = $video['name_cs'];
+        $videoNew['name_en'] = $video['name_en'];
+        $videoNew['tags'] = $video['tags'];
+        $videoNew['views'] = $video['views'];
+        $videoNew['categories'] = $video['categories'];
+        $videoNew['tags'] = $video['tags'];
+        $videoNew['description_cs'] = $video['description_cs'];
+        $videoNew['description_en'] = $video['description_en'];
+        $videoNew['note'] = $video['note'];
+        
         
         $this->template->videoNew = $videoNew;
     }
