e90a932869f3d905302bee3c09d5ce90e2be56fb
Michal Mlejnek <chaemil72@gmail.com>
2015-10-08 13:48:20 +0200

    added try catch

--- web-project/app/Utils/ImageUtils.php
+++ web-project/app/Utils/ImageUtils.php
@@ -18,29 +18,34 @@ use App\StringUtils,
  */
 class ImageUtils {
     public static function resizeImage($dir, $imagefile, $size, $postfix, $outputDir) {
-        $image = Image::fromFile($dir."/".$imagefile);
-        $image->resize($size,NULL,Image::SHRINK_ONLY);
-        $imagefile_without_extension = StringUtils::removeExtensionFromFileName($imagefile);
-        $extension = StringUtils::getExtensionFromFileName($imagefile);
-        if ($postfix != "") {
-            $separator = "_";
-        } else {
-            $separator = "";
+        if (filesize($dir."/".$imagefile) != 0) {
+            try{
+                $image = Image::fromFile($dir."/".$imagefile);
+                $image->resize($size,NULL,Image::SHRINK_ONLY);
+                $imagefile_without_extension = StringUtils::removeExtensionFromFileName($imagefile);
+                $extension = StringUtils::getExtensionFromFileName($imagefile);
+                if ($postfix != "") {
+                    $separator = "_";
+                } else {
+                    $separator = "";
+                }
+                if (!empty($outputDir)) {
+                    $dir = $outputDir;
+                }
+
+                switch($extension) {
+                    case 'jpg':
+                        $image->save($dir."/".$imagefile_without_extension.$separator.$postfix.".".$extension, 
+                            80, Image::JPEG);
+                        break;
+                    case 'png':
+                        $image->save($dir."/".$imagefile_without_extension.$separator.$postfix.".".$extension, 
+                            100, Image::PNG);
+                        break;
+            }
+        } catch(Exception $e) {
+            Debugger::log($e);
+            }
         }
-        if (!empty($outputDir)) {
-            $dir = $outputDir;
-        }
-        
-        switch($extension) {
-            case 'jpg':
-                $image->save($dir."/".$imagefile_without_extension.$separator.$postfix.".".$extension, 
-                    80, Image::JPEG);
-                break;
-            case 'png':
-                $image->save($dir."/".$imagefile_without_extension.$separator.$postfix.".".$extension, 
-                    100, Image::PNG);
-                break;
-        }
-        
     }
 }
