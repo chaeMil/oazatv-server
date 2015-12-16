4a24694e94391ed2d5372bbe702c2e8244dc0cb8
chäeMil <chaemil72@gmail.com>
2015-12-16 15:57:45 +0100

    added basic search api

---
+++ web-project/app/ApiModule/presenters/SearchPresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SearchPresenter
+ *
+ * @author chaemil
+ */
+
+namespace App\ApiModule;
+
+use Nette,
+ Nette\Application\Responses\JsonResponse,
+ Model\SearchManager,
+ Nette\Database\Context;
+
+class SearchPresenter extends BasePresenter {
+    
+    private $searchManager;
+    
+    public function __construct(Nette\DI\Container $container,
+            Context $database, SearchManager $searchManager) {
+        
+        parent::__construct($container, $database);
+        $this->searchManager = $searchManager;
+    }
+    
+    public function renderDefault($id) {
+        
+        $input = $id;
+        
+        $response = $this->searchManager->search($input, $this->lang);
+                       
+        $this->sendResponse(new JsonResponse($response));
+        
+    }
+    
+}
