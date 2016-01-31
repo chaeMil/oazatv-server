8e4a18d6fa0ce71cfc5a49430041e3d814a4b7b0
Michal Mlejnek <chaemil72@gmail.com>
2016-01-31 17:13:23 +0100

    added songs manager and presenter

---
+++ web-project/app/AdminModule/presenters/SongsPresenter.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\AdminModule;
+
+use Nette,
+ Model\SongsManager,
+ App\EventLogger;
+
+/**
+ * Description of VideoPresenter
+ *
+ * @author chaemil
+ */
+class SongsPresenter extends BaseSecuredPresenter {
+    
+    public $database;
+    public $songsManager;
+
+    function __construct(Nette\Database\Context $database, 
+            SongsManager $songsManager) {
+        $this->database = $database;
+        $this->songsManager = $songsManager;
+    }
+    
+    public function renderList() {       
+        $this->getTemplateVariables($this->getUser()->getId());
+        
+        $this->template->songs = $this->songsManager
+                ->getSongsFromDB();
+    }
+    
+    public function renderCreateSong() {
+        $this->getTemplateVariables($this->getUser()->getId());
+    }
+    
+    public function renderDetail($id) {
+        $this->getTemplateVariables($this->getUser()->getId());
+        $song = $this->songsManager->getSongFromDB($id);
+        $this->template->song = $song;
+        
+        if (!isset($song['id'])) {
+            $this->error('Požadovaná chvála neexistuje!');
+        }
+        
+        $this['songForm']->setDefaults($song->toArray());
+        
+    }
+    
+    public function createComponentCategoryForm() {        
+        $form = new Nette\Application\UI\Form;
+        
+        $form->addHidden('id');
+        
+        $form->addText('name', 'název')
+                ->setAttribute("class", "form-control")
+                ->setRequired();
+
+        $form->addText('tag', 'TAG')
+                ->setRequired()
+                ->setAttribute("class", "form-control");
+
+        $form->addSubmit('send', 'Uložit')
+                ->setAttribute("class", "btn-lg btn-success btn-block");
+        
+
+        // call method signInFormSucceeded() on success
+        $form->onSuccess[] = $this->songSucceeded;
+
+        // setup Bootstrap form rendering
+        $this->bootstrapFormRendering($form);
+
+        return $form;
+    }
+    
+    
+    public function songSucceeded($form) {
+        $vals = $form->getValues();
+        
+        $status = $this->songsManager->saveSongToDB($vals);
+        
+        if ($status) {
+            EventLogger::log('user '.$this->getUser()->getIdentity()
+                    ->login.' updated song '.$vals->id,
+                EventLogger::ACTIONS_LOG);
+            
+            $this->flashMessage("Změny úspěšně uloženy", "success");
+        } else {
+            $this->flashMessage("Nic nebylo změněno", "info");
+        }
+    }
+    
+    public function actionDeleteSong($id) {
+        $this->songsManager->deleteSong($id);
+        EventLogger::log('user '.$this->getUser()->getIdentity()->login.' deleted song '.$id, 
+                EventLogger::ACTIONS_LOG);
+        $this->flashMessage("Chvála byla smazána!", "danger");
+        $this->redirect("Songs:List");
+    }
+}
+
---
+++ web-project/app/model/SongsManager.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CategoriesManager
+ *
+ * @author Michal Mlejnek <chaemil72 at gmail.com>
+ */
+namespace Model;
+
+use Nette;
+/**
+ * Description of VideoManager
+ *
+ * @author chaemil
+ */
+class SongsManager extends BaseModel {
+
+    const
+            TABLE_NAME = 'db_known_songs',
+            COLUMN_ID = 'id',
+            COLUMN_TAG = 'tag',
+            COLUMN_NAME_CS = 'name_';
+
+    /** @var Nette\Database\Context */
+    public static $database;
+
+    public function __construct(Nette\Database\Context $database) {
+        self::$database = $database;
+    }
+    
+    private function checkIfSongExists($id) {
+        return self::$database->table(self::TABLE_NAME)
+                ->where(self::COLUMN_ID, $id)->count();
+    }
+    
+    public function saveSongToDB($values) {
+
+        if(isset($values['id'])) {
+            $id = \Nette\Utils\Strings::webalize($values['id']);
+        } else {
+            $id = 0;
+        }
+
+        if ($id != 0 && $this->checkIfSongExists($id) > 0) {
+            $category = self::$database->table(self::TABLE_NAME)->get($id);
+            $sql = $category->update($values);
+            return $sql;
+        } else {
+            $sql = self::$database->table(self::TABLE_NAME)->insert($values);
+        }
+
+        return $sql->id;
+
+    }
+    
+    
+    public function getSongFromDB($id) {
+        return self::$database->table(self::TABLE_NAME)
+                ->select("*")
+                ->where(array(self::COLUMN_ID => $id))
+                ->fetch();
+    }
+    
+    public function getSongsFromDB() {
+        return self::$database->table(self::TABLE_NAME)
+            ->select('*')
+            ->fetchAll();
+
+    }
+    
+    public function deleteSong($id) {
+        $video = $this->getSongFromDB($id);
+        $video->delete();
+    }
+   
+}
