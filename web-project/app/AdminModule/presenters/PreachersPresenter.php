c10034237fd7081001f6647996b57200dbfa1ebc
Michal Mlejnek <chaemil72@gmail.com>
2016-03-14 21:54:38 +0100

    add preachers presnter and tempaltes

---
+++ web-project/app/AdminModule/presenters/PreachersPresenter.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\AdminModule;
+
+use Nette,
+ Model\PreachersManager,
+ App\EventLogger;
+
+/**
+ * Description of VideoPresenter
+ *
+ * @author chaemil
+ */
+class PreachersPresenter extends BaseSecuredPresenter {
+    
+    public $database;
+    public $preachersManager;
+
+    function __construct(Nette\Database\Context $database, 
+        PreachersManager $preachersManager) {
+        $this->database = $database;
+        $this->preachersManager = $preachersManager;
+    }
+    
+    public function renderList() {       
+        $this->getTemplateVariables($this->getUser()->getId());
+        
+        $this->template->preachers = $this->preachersManager
+                ->getPreachersFromDB();
+    }
+    
+    public function renderCreatePreacher() {
+        $this->getTemplateVariables($this->getUser()->getId());
+    }
+    
+    public function renderDetail($id) {
+        $this->getTemplateVariables($this->getUser()->getId());
+        $preacher = $this->preachersManager->getPreacherFromDB($id);
+        $this->template->preacher = $preacher;
+        
+        if (!isset($preacher['id'])) {
+            $this->error('Požadovaný kazatel neexistuje!');
+        }
+        
+        $this['preacherForm']->setDefaults($preacher->toArray());
+        
+    }
+    
+    public function createComponentPreacherForm() {        
+        $form = new Nette\Application\UI\Form;
+        
+        $form->addHidden('id');
+        
+        $form->addText('name', 'jméno')
+                ->setAttribute("class", "form-control")
+                ->setRequired();
+
+        $form->addText('tags', 'tagy')
+                ->setRequired()
+                ->setAttribute("class", "form-control");
+        
+        $form->addUpload('file', 'fotka')
+                ->setAttribute("class", "form-control");
+        
+        $form->addTextArea('about_cs', 'info česky')
+                ->setHtmlId('aboutCsEditor')
+                ->setAttribute("class", "form-control");
+        
+        $form->addTextArea('about_en', 'info anglicky')
+                ->setHtmlId('aboutEnEditor')
+                ->setAttribute("class", "form-control");
+
+        $form->addSubmit('send', 'Uložit')
+                ->setAttribute("class", "btn-lg btn-success btn-block");
+        
+
+        $form->onSuccess[] = $this->preacherSucceeded;
+
+        // setup Bootstrap form rendering
+        $this->bootstrapFormRendering($form);
+
+        return $form;
+    }
+    
+    
+    public function preacherSucceeded($form) {
+        $vals = $form->getValues();
+        
+        $status = $this->preachersManager->savePreacherToDB($vals);
+        
+        if ($status) {
+            EventLogger::log('user '.$this->getUser()->getIdentity()
+                    ->login.' updated preacher '.$vals->id,
+                EventLogger::ACTIONS_LOG);
+            
+            $this->flashMessage("Změny úspěšně uloženy", "success");
+        } else {
+            $this->flashMessage("Nic nebylo změněno", "info");
+        }
+    }
+    
+    public function actionDeletePreacher($id) {
+        $this->preachersManager->deletePreacher($id);
+        EventLogger::log('user '.$this->getUser()->getIdentity()->login.' deleted preacher '.$id, 
+                EventLogger::ACTIONS_LOG);
+        $this->flashMessage("Kazatel byl smazán!", "danger");
+        $this->redirect("Preachers:List");
+    }
+}
+
