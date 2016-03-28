5479f2918c6b58dd0e59af8d1b501c3fb109660d
Michal Mlejnek <chaemil72@gmail.com>
2016-03-28 15:40:43 +0200

    add nothing found messages

--- web-project/app/FrontModule/presenters/SearchPresenter.php
+++ web-project/app/FrontModule/presenters/SearchPresenter.php
@@ -24,26 +24,33 @@ class SearchPresenter extends BasePresenter {
         $limit = 32;
         $search = $this->searchManager->search($q, 999);
         
-        $mergedSearch = array_merge($search['videos'], $search['albums']);
-        usort($mergedSearch, array($this, 'sortItemsByDate'));
-        
-        $searchPage = $page;
-        if ($page != 0) {
-            $searchPage = $page -1;
-        }
-        $mergedLimitedSearch = array_slice($mergedSearch, $searchPage * $limit, $limit);
-        
-        $paginator = new Nette\Utils\Paginator;
-        $paginator->setItemsPerPage($limit);
-        $paginator->setPage($page);
-        $paginator->setItemCount(count($mergedSearch));
+        if(isset($search['videos']) || isset($search['albums'])) {
+            if(count($search['videos']) != 0 || count($search['albums'] != 0)) {
 
-        $this->template->q = $q;
-        $this->template->search = $mergedLimitedSearch;
-        $this->template->paginator = $paginator;
-        $this->template->page = $paginator->getPage();
-        $this->template->pages = $paginator->getPageCount();
-        $this->template->itemsPerPage = $paginator->getItemsPerPage();
+                $mergedSearch = array_merge($search['videos'], $search['albums']);
+                usort($mergedSearch, array($this, 'sortItemsByDate'));
+
+                $searchPage = $page;
+                if ($page != 0) {
+                    $searchPage = $page -1;
+                }
+                $mergedLimitedSearch = array_slice($mergedSearch, $searchPage * $limit, $limit);
+                
+                $paginator = new Nette\Utils\Paginator;
+                $paginator->setItemsPerPage($limit);
+                $paginator->setPage($page);
+                $paginator->setItemCount(count($mergedSearch));
+            }
+        }
+        
+        if (isset($mergedLimitedSearch)) {
+            $this->template->q = $q;
+            $this->template->search = $mergedLimitedSearch;
+            $this->template->paginator = $paginator;
+            $this->template->page = $paginator->getPage();
+            $this->template->pages = $paginator->getPageCount();
+            $this->template->itemsPerPage = $paginator->getItemsPerPage();
+        }
         
     }
     
