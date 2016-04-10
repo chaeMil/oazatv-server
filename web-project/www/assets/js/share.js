28f1889aa9b11251ec5a3034c639fd88165d77a0
Michal Mlejnek <chaemil72@gmail.com>
2016-04-10 21:28:48 +0200

    add sharing of url

--- web-project/www/assets/js/share.js
+++ web-project/www/assets/js/share.js
@@ -1,3 +1,11 @@
-function shareLink() {
-    
+function shareLink(title) {
+    bootbox.prompt({
+        title: title,
+        value: window.location.href,
+        callback: function (result) {
+            if (result !== null) {
+                // copyToClipboard($('input.bootbox-input-text')); TODO
+            }
+        }
+    });
 }
