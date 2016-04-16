d757f9aa5389c3c8b369cb695d914d58b7d3cd12
Michal Mlejnek <chaemil72@gmail.com>
2016-04-16 23:12:57 +0200

    add terminate to response

--- web-project/app/ApiModule/presenters/BasePresenter.php
+++ web-project/app/ApiModule/presenters/BasePresenter.php
@@ -124,5 +124,6 @@ class BasePresenter extends \Nette\Application\UI\Presenter {
     public function sendHTTPResponse($response) {
         $httpResponse = $this->container->getByType('Nette\Http\Response');
         $httpResponse->setCode($response);
+        exit;
     }
 }
