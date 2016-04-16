18cc6d2059e866c6eff34598c587aa8d45ffdc90
Michal Mlejnek <chaemil72@gmail.com>
2016-04-16 20:52:08 +0200

    fix query

--- web-project/app/model/AnalyticsManager.php
+++ web-project/app/model/AnalyticsManager.php
@@ -120,7 +120,7 @@ class AnalyticsManager {
         return $this->database->query("SELECT * FROM ".self::TABLE_NAME_ANALYTICS_ALIVE_USERS." WHERE ".
                         self::COLUMN_PAGE.' LIKE "%'.$page.'%" AND '.
                         self::COLUMN_ALIVE.' >= DATE_SUB(NOW(),INTERVAL '.(1 * $minutes).' MINUTE) AND '.
-                        self::COLUMN_OAZA_USER_ID.' != '.$adminUserOazaId)
+                        self::COLUMN_OAZA_USER_ID.' != "'.$adminUserOazaId.'"')
                 ->fetchAll();
     }
     
