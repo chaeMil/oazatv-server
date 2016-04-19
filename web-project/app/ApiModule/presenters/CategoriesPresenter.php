50813437d3511ed5ebd4b239a9972e5870600e4e
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2016-04-19 19:10:59 +0200

    add categories presenter

---
+++ web-project/app/ApiModule/presenters/CategoriesPresenter.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\ApiModule;
+
+use Nette,
+ Model\CategoriesManager,
+ Model\VideoManager;
+
+/**
+ * Description of CategoriesPresenter
+ *
+ * @author chaemil
+ */
+class CategoriesPresenter extends BasePresenter{
+    
+    public $database;
+    public $categoriesManager;
+    public $videoManager;
+
+    function __construct(Nette\Database\Context $database, 
+            CategoriesManager $categoriesManager, VideoManager $videoManager) {
+        $this->database = $database;
+        $this->categoriesManager = $categoriesManager;
+        $this->videoManager = $videoManager;
+    }
+    
+    public function actionDefault() {
+        
+        $categories = $this->categoriesManager->getCategoriesFromDB();
+        
+        $categoriesArray = array();
+        
+        foreach($categories as $category) {
+            
+            $categoryJson['category'] = $category;
+            $categoryJson['videos'] = array();
+            
+            $videosFromCategory = $this->videoManager->getVideosFromDBbyCategory($category['id'], 0, 120);
+            
+            foreach($videosFromCategory as $video) {
+                $categoryJson['videos'][] = $this->videoManager->getVideoFromDBtoAPI($video['id']);
+            }
+        }
+        
+        $this->sendJson($categoriesArray);
+        
+    }
+    
+}
