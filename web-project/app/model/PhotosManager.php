1704801ce64abce49c572796502705e68f22edb1
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2016-05-04 20:34:21 +0200

    rollback

--- web-project/app/model/PhotosManager.php
+++ web-project/app/model/PhotosManager.php
@@ -188,13 +188,8 @@ class PhotosManager {
     public function getPhotoThumbnails($photoId) {
         $photo = $this->getPhotoFromDB($photoId);
         if ($photo) {
-            $thumbsLocation = ALBUMS_FOLDER.$photo->album_id.'/thumbs/';
             $thumbLocation = ALBUMS_FOLDER.$photo->album_id.'/thumbs/'.str_replace(".jpg", "_".self::THUMB_1024.".jpg", $photo->file);
             if (!file_exists($thumbLocation)) {
-                if (!file_exists($thumbsLocation)) {
-                    mkdir($thumbsLocation);
-                    chmod($thumbsLocation, 0777);
-                }
                 mkdir($thumbLocation);
                 chmod($thumbLocation, 0777);
             }            
