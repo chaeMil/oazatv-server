4091570d6cdb91bc49c3f577ef1300ee6e38df6b
Michal Mlejnek <michal.mlejnek@juicymo.cz>
2016-05-04 20:26:01 +0200

    add thumb folder creation

--- web-project/app/model/PhotosManager.php
+++ web-project/app/model/PhotosManager.php
@@ -188,8 +188,11 @@ class PhotosManager {
     public function getPhotoThumbnails($photoId) {
         $photo = $this->getPhotoFromDB($photoId);
         if ($photo) {
+            $thumbsLocation = ALBUMS_FOLDER.$photo->album_id.'/thumbs/';
             $thumbLocation = ALBUMS_FOLDER.$photo->album_id.'/thumbs/'.str_replace(".jpg", "_".self::THUMB_1024.".jpg", $photo->file);
             if (!file_exists($thumbLocation)) {
+                mkdir($thumbsLocation);
+                chmod($thumbsLocation, 0777);
                 mkdir($thumbLocation);
                 chmod($thumbLocation, 0777);
             }            
