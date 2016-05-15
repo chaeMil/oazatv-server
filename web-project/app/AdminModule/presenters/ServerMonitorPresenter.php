ad647db9a555c304b3dc7e7212f003aa40a4876f
Michal Mlejnek <chaemil72@gmail.com>
2016-05-15 21:38:24 +0200

    add simple cpu temp and load

--- web-project/app/AdminModule/presenters/ServerMonitorPresenter.php
+++ web-project/app/AdminModule/presenters/ServerMonitorPresenter.php
@@ -9,7 +9,8 @@
 namespace App\AdminModule;
 
 use Nette,
- Model\ServerMonitorManager;
+ Model\ServerMonitorManager,
+ Nette\Application\Responses\JsonResponse;
 
 /**
  * Description of ServerMonitorPresenter
@@ -19,18 +20,51 @@ use Nette,
 class ServerMonitorPresenter  extends BaseSecuredPresenter {
     
     public $database;
+    private $container;
     private $serverMonitorManager;
     
     function __construct(Nette\Database\Context $database,
+            Nette\DI\Container $container,
             ServerMonitorManager $serverMonitorManager) {
         $this->database = $database;
+        $this->container = $container;
         $this->serverMonitorManager = $serverMonitorManager;
     }
 
     
     public function renderDefault() {
         $this->getTemplateVariables($this->getUser()->getId());
+
+        $this->template->dataVolume = $this->getDataVolume();
+        $this->template->systemVolume = $this->getSystemVolume();
+        $this->template->server = SERVER;
+    }
+    
+    public function actionAjaxData() {
+        $cpuTemp = $this->serverMonitorManager->getCPUTemp(true);
+        $cpuLoad = $this->serverMonitorManager->getCPULoad();
+        $dataVolume = $this->getDataVolume();
+        $systemVolume = $this->getSystemVolume();
         
+        $response['cpu'] = array('cpuTemp' => $cpuTemp, 'cpuLoad' => $cpuLoad);
+        $response['volumes'] = array('dataVolume' => $dataVolume, 'systemVolume' => $systemVolume);
+        
+        $this->sendResponse(new JsonResponse($response));
+    }
+    
+    public function actionSaveCpuLog() {
+        $this->serverMonitorManager->saveCpuLog();
+        $httpResponse = $this->container->getByType('Nette\Http\Response');
+        $httpResponse->setCode(\Nette\Http\Response::S200_OK);
+        $this->terminate();
+    }
+    
+    public function renderLoadCpuLog($items) {
+        $cpuLog = $this->serverMonitorManager->getCpuLog($items);
+        $this->sendResponse(new JsonResponse($cpuLog));
+    }
+    
+    private function getDataVolume() {
         $dataVolume['used'] = $this->serverMonitorManager->getUsedDataDiskSpace();
         $dataVolume['total'] = $this->serverMonitorManager->getTotalDataDiskSpace();
         $dataVolume['free'] = $this->serverMonitorManager->getFreeDataDiskSpace();
@@ -39,6 +73,10 @@ class ServerMonitorPresenter  extends BaseSecuredPresenter {
         $dataVolume['totalH'] = $this->serverMonitorManager->getTotalDataDiskSpace(true);
         $dataVolume['freeH'] = $this->serverMonitorManager->getFreeDataDiskSpace(true);
         
+        return $dataVolume;
+    }
+    
+    private function getSystemVolume() {
         $systemVolume['used'] = $this->serverMonitorManager->getUsedSystemDiskSpace();
         $systemVolume['total'] = $this->serverMonitorManager->getTotalsystemDiskSpace();
         $systemVolume['free'] = $this->serverMonitorManager->getFreeSystemDiskSpace();
@@ -47,8 +85,6 @@ class ServerMonitorPresenter  extends BaseSecuredPresenter {
         $systemVolume['totalH'] = $this->serverMonitorManager->getTotalsystemDiskSpace(true);
         $systemVolume['freeH'] = $this->serverMonitorManager->getFreeSystemDiskSpace(true);
         
-        $this->template->dataVolume = $dataVolume;
-        $this->template->systemVolume = $systemVolume;
+        return $systemVolume;
     }
-    
 }
--- web-project/app/model/ServerMonitorManager.php
+++ web-project/app/model/ServerMonitorManager.php
@@ -17,6 +17,20 @@ use Nette,
  * @author chaemil
  */
 class ServerMonitorManager {
+    
+    const
+            TABLE_NAME_TEMP = 'log_cpu_temp',
+            TABLE_NAME_LOAD = 'log_cpu_load',
+            COLUMN_TIMESTAMP = 'timestamp',
+            COLUMN_LOAD = 'cpu_load',
+            COLUMN_TEMP = 'temp';
+    
+    /** @var Nette\Database\Context */
+    public static $database;
+
+    public function __construct(Nette\Database\Context $database) {
+        self::$database = $database;
+    }
 
     public function getTotalDataDiskSpace($humanReadable = false) {
         if ($humanReadable) {
@@ -90,4 +104,59 @@ class ServerMonitorManager {
         return exec('df -aT'.$h.' | grep '.SYSTEM_MOUNTPOINT.' | head -n1 | awk \'{print $6}\'');
     }
     
+    public function getCPUTemp($intVal = false) {
+        $exec = exec(CPU_TEMP_COMMAND);;
+        if ($intVal) {
+            return preg_replace('/[^a-zA-Z]/', '', $exec);
+        } else {
+            return $exec;
+        }
+    }
+    
+    public function getCpuLoad() {
+        return exec(CPU_LOAD_COMMAND);
+    }
+    
+    public function saveCpuLog() {
+        $cpuLoad = $this->getCpuLoad();
+        $cpuTemp = $this->getCPUTemp(true);
+        
+        if (!is_numeric($cpuLoad)) {
+            $cpuLoad = 0;
+        }
+        self::$database->table(self::TABLE_NAME_LOAD)
+                ->insert(array(self::COLUMN_LOAD => $cpuLoad));
+        
+        if (!is_numeric($cpuTemp)) {
+            $cpuTemp = 0;
+        }
+        self::$database->table(self::TABLE_NAME_TEMP)
+                ->insert(array(self::COLUMN_TEMP => $cpuTemp));
+    }
+    
+    public function getCpuLog($items = 30) {
+        $cpuLoad = self::$database->table(self::TABLE_NAME_LOAD)
+                ->select('*')
+                ->order(self::COLUMN_TIMESTAMP)
+                ->limit($items)
+                ->fetchPairs(self::COLUMN_TIMESTAMP, self::COLUMN_LOAD);
+        
+        $cpuTemp = self::$database->table(self::TABLE_NAME_TEMP)
+                ->select('*')
+                ->order(self::COLUMN_TIMESTAMP)
+                ->limit($items)
+                ->fetchPairs(self::COLUMN_TIMESTAMP, self::COLUMN_TEMP);
+        
+        $cpuLoadOut = array();
+        foreach($cpuLoad as $key => $load) {
+            $cpuLoadOut['data'][] = array(strtotime($key), $load);
+        }
+        
+        $cpuTempOut = array();
+        foreach($cpuTemp as $key => $temp) {
+            $cpuTempOut['data'][] = array(strtotime($key), $temp);
+        }
+        
+        return array('temp' => $cpuTempOut, 'load' => $cpuLoadOut);
+    }
 }
