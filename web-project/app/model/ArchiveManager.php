914502291c7c7f2884344cf2132d4aacb2708ba5
Michal Mlejnek <chaemil72@gmail.com>
2016-06-11 13:20:44 +0200

    Fix archive order

--- web-project/app/model/ArchiveManager.php
+++ web-project/app/model/ArchiveManager.php
@@ -44,7 +44,7 @@ class ArchiveManager extends BaseModel {
                 ", 'album' AS type FROM ".
                 PhotosManager::TABLE_NAME_ALBUMS.
                 " WHERE ".PhotosManager::COLUMN_PUBLISHED." = 1 ".
-                ") ORDER BY ".$order.
+                ") ORDER BY ".$order. " created_at DESC".
                 " LIMIT ".$count.
                 " OFFSET ".$from;
 
